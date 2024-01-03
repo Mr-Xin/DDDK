fc8afd5651fbf5d532e4cb9b3dbdcf11b7efffd5
xinfeng <xf93328@163.com>
2024-01-03 21:48:08 +0800

    push

---
+++ DDDK-FTZX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDDK-FTZX
+//
+//  Created by xinfeng on 2017/11/6.
+//  Copyright © 2017年 DDDK-FTZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DDDK-FTZXTests/DDDK_FTZXTests.m
@@ -0,0 +1,39 @@
+//
+//  DDDK_FTZXTests.m
+//  DDDK-FTZXTests
+//
+//  Created by xinfeng on 2017/11/6.
+//  Copyright © 2017年 DDDK-FTZX. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DDDK_FTZXTests : XCTestCase
+
+@end
+
+@implementation DDDK_FTZXTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
